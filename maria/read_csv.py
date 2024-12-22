17ddca4839bb977525d46ea73633c1de6b3e560e
chryssa-pat <83291272+chryssa-pat@users.noreply.github.com>
2024-12-22 10:55:50 +0200

    Create read_csv.py

---
+++ maria/read_csv.py
@@ -0,0 +1,15 @@
+from pyspark.sql import SparkSession
+
+# Δημιουργία SparkSession
+spark = SparkSession.builder \
+    .appName("ReadBikeStats") \
+    .getOrCreate()
+
+# Διάβασμα όλων των CSV από όλες τις ημέρες
+all_data_spark = spark.read \
+    .option("header", "true") \
+    .option("inferSchema", "true") \
+    .csv("bike_usage_statistics/date=*")
+
+# Εμφάνιση των δεδομένων
+all_data_spark.show()
