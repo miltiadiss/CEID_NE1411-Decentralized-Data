82bb2b6180de1e7aa80b7dcde836d933bb479ff1
mariak0 <83291320+mariak0@users.noreply.github.com>
2024-12-29 17:33:39 +0200

    Update venv.py

--- maria/venv.py
+++ maria/venv.py
@@ -7,4 +7,4 @@ pip install jsonschema
 pip install confluent-kafka
 pip install pyspark confluent-kafka
 
-spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.2,org.apache.spark:spark-streaming-kafka-0-10_2.12:3.5.2 spark.py
+
